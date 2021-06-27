c9827a4ad882442e550d10d15dfa47c62cf91b51
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-06-27 11:34:42 +0300

    add: session

---
+++ src/main/java/com/peregud/springmvc/session/SessionListenerController.java
@@ -0,0 +1,37 @@
+package com.peregud.springmvc.session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RestController
+public class SessionListenerController {
+    private static final Logger LOG = LoggerFactory.getLogger(SessionListenerController.class);
+
+    @GetMapping("/create-new-session")
+    public String createNewSession(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            LOG.info("Session not available, creating new session");
+            session = request.getSession(true);
+        }
+        return "Session is available now: " + session;
+    }
+
+    @GetMapping("/destroy-active-session")
+    public String removeSession(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+            return "Session Destroyed: " + session;
+        }
+        return "Session is not available to destroy";
+    }
+}
